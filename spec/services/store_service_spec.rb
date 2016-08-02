9ef2921f2f752194d8f998a6df482fb87a50b948
Lucy Fox <lucyreneefox@gmail.com>
2016-08-02 11:57:18 -0600

    Add less then 25 miles expectations in service spec

--- spec/services/store_service_spec.rb
+++ spec/services/store_service_spec.rb
@@ -8,9 +8,11 @@ describe "Store Service" do
       results = StoreService.new.stores_by_zip(zip)
 
       stores = results["stores"]
+      distances = stores.map {|store| store["distance"]}
 
       expect(results["total"]).to eq(17)
       expect(stores.count).to eq(15)
+      expect(distances.last).to be <= 25
       expect(stores.first).to eq({"longName"=>"Best Buy Mobile - Cherry Creek Shopping Center", "city"=>"Denver", "distance"=>3.25, "phone"=>"303-270-9189", "storeType"=>"Mobile", "storeId"=>2740})
     end
   end
