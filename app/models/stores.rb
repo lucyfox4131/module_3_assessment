5057f51635765c853006307cfa8a383f52141bac
Lucy Fox <lucyreneefox@gmail.com>
2016-08-02 10:43:23 -0600

    First user story complete

--- app/models/stores.rb
+++ app/models/stores.rb
@@ -2,20 +2,23 @@ class Stores
   attr_reader :name, :city, :distance, :phone, :type
 
   def initialize(store)
-    @name = store["longName"]
-    @city = store["city"]
-    @distance = store["distance"]
-    @phone = store["phone"]
-    @type = store["storeType"]
+    @name       = store["longName"]
+    @city       = store["city"]
+    @distance   = store["distance"]
+    @phone      = store["phone"]
+    @type       = store["storeType"]
   end
 
   def self.search_by_zip(zip)
     results = StoreService.new.stores_by_zip(zip)
-
-    stores = results["stores"].map do |store|
-      new(store)
-    end
+    stores = create_new_stores(results)
 
     [results["total"], stores]
+  end
+
+  def self.create_new_stores(results)
+    results["stores"].map do |store|
+      new(store)
+    end
   end
 end
--- spec/features/user_searches_by_zip_spec.rb
+++ spec/features/user_searches_by_zip_spec.rb
@@ -8,16 +8,31 @@ RSpec.feature "User searches by zip for stores" do
 
     expect(current_path).to eq("/search")
 
-    expect(page).to have_content("Best Buy Mobile - Cherry Creek Shopping Center")
+    expect(page).to have_link("Best Buy Mobile - Cherry Creek Shopping Center")
     expect(page).to have_content("17 Total Stores")
 
-    within(".store15") do
-      expect(page).to have_content("Best Buy - S.E. Aurora")
-      expect(page).to have_content("City: Aurora")
-      expect(page).to have_content("Distance: 18.51")
-      expect(page).to have_content("Phone Number: 303-693-0166")
-      expect(page).to have_content("Store Type: BigBox")
-    end
+    expect(page).to have_link("Best Buy - S.E. Aurora")
+    expect(page).to have_content("City: Aurora")
+    expect(page).to have_content("Distance: 18.51")
+    expect(page).to have_content("Phone Number: 303-693-0166")
+    expect(page).to have_content("Store Type: BigBox")
+
+
   end
 
 end
+
+#
+# As a user
+# After I have searched a zip code for stores
+# When I click the name of a store
+# Then my current path should be "/stores/:store_id"
+# I should see the store name, store type and address with city, state and zip
+# I should see an unordered list of the store hours in the following format:
+#   * Mon: 10am-9pm
+#   * Tue: 10am-9pm
+#   * Wed: 10am-9pm
+#   * Thurs: 10am-9pm
+#   * Fri: 10am-9pm
+#   * Sat: 10am-9pm
+#   * Sun: 11am-7pm
